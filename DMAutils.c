7ec2962bfc4e68d659bc0e84d205c23b7b08c752
TMaxElectronics <60486146+TMaxElectronics@users.noreply.github.com>
2023-01-13 01:54:39 +0100

    Added output ringbuffer

--- DMAutils.c
+++ DMAutils.c
@@ -7,11 +7,17 @@
 #include "FreeRTOS.h"
 #include "stream_buffer.h"
 #include "DMAutils.h"
+#include "../DMAconfig.h"
 #include "System.h"
 
 
-DMA_RINGBUFFERHANDLE_t * DMA_createRingBuffer(uint32_t bufferSize, uint32_t dataSize, uint32_t * dataSrc, uint32_t dataReadyInt, uint32_t prio){
+DMA_RINGBUFFERHANDLE_t * DMA_createRingBuffer(uint32_t bufferSize, uint32_t dataSize, uint32_t * dataSrc, uint32_t dataReadyInt, uint32_t prio, uint32_t direction){
     DMA_RINGBUFFERHANDLE_t * ret = pvPortMalloc(sizeof(DMA_RINGBUFFERHANDLE_t));
+    
+    ret->direction = direction;
+    ret->lastReadPos = 0;
+    ret->bufferSize = bufferSize;
+    ret->dataSize = dataSize;
     
     ret->channelHandle = DMA_allocateChannel();
     
@@ -19,19 +25,28 @@ DMA_RINGBUFFERHANDLE_t * DMA_createRingBuffer(uint32_t bufferSize, uint32_t data
         vPortFree(ret);
         return NULL;
     }
-    
+        
     DMA_setChannelAttributes(ret->channelHandle, 0, 0, 0, 1, prio);
     DMA_setInterruptConfig(ret->channelHandle, 0,0,0,0,0,0,0,0);
     DMA_setTransferAttributes(ret->channelHandle, dataSize, dataReadyInt, -1);
-    DMA_setSrcConfig(ret->channelHandle, dataSrc, dataSize);
+        
+    if(direction == RINGBUFFER_DIRECTION_RX){
     
-    ret->data = SYS_makeCoherent(pvPortMalloc(bufferSize));
-    
-    DMA_setDestConfig(ret->channelHandle, ret->data, bufferSize);
-    
-    ret->lastReadPos = 0;
-    ret->bufferSize = bufferSize;
-    ret->dataSize = dataSize;
+        DMA_setSrcConfig(ret->channelHandle, dataSrc, dataSize);
+        ret->data = SYS_makeCoherent(pvPortMalloc(bufferSize));
+        DMA_setDestConfig(ret->channelHandle, ret->data, bufferSize);
+        
+    }else if(direction == RINGBUFFER_DIRECTION_TX){
+        
+        DMA_setDestConfig(ret->channelHandle, dataSrc, dataSize);
+        ret->data = SYS_makeCoherent(pvPortMalloc(bufferSize));
+        DMA_setSrcConfig(ret->channelHandle, ret->data, bufferSize);
+        
+    }else{
+        DMA_freeChannel(ret->channelHandle);
+        vPortFree(ret);
+        return NULL;
+    }
     
     return ret;
 }
@@ -45,15 +60,26 @@ void DMA_freeRingBuffer(DMA_RINGBUFFERHANDLE_t * handle){
     vPortFree(handle);
 }
 
+//returns either the amount of data available for reading out of amount of data available for the dma to write to the target
 uint32_t DMA_RB_available(DMA_RINGBUFFERHANDLE_t * handle){
-    if(*(handle->channelHandle->DPTR) >= handle->lastReadPos){
-        return *(handle->channelHandle->DPTR) - handle->lastReadPos;
+    if(handle->direction == RINGBUFFER_DIRECTION_RX){
+        if(*(handle->channelHandle->DPTR) >= handle->lastReadPos){
+            return *(handle->channelHandle->DPTR) - handle->lastReadPos;
+        }else{
+            return *(handle->channelHandle->DPTR) + handle->bufferSize - handle->lastReadPos;
+        }
     }else{
-        return *(handle->channelHandle->DPTR) + handle->bufferSize - handle->lastReadPos;
+        if(*(handle->channelHandle->DPTR) <= handle->lastReadPos){
+            return handle->lastReadPos - *(handle->channelHandle->DPTR);
+        }else{
+            return handle->lastReadPos + handle->bufferSize - *(handle->channelHandle->DPTR);
+        }
     }
 }
 
 uint32_t DMA_RB_read(DMA_RINGBUFFERHANDLE_t * handle, uint32_t * dst, uint32_t size){
+    if(handle->direction != RINGBUFFER_DIRECTION_RX) return 0;
+    
     uint32_t available = DMA_RB_available(handle);
     if(size > available) size = available;
     
@@ -66,7 +92,23 @@ uint32_t DMA_RB_read(DMA_RINGBUFFERHANDLE_t * handle, uint32_t * dst, uint32_t s
     return size;
 }
 
+uint32_t DMA_RB_write(DMA_RINGBUFFERHANDLE_t * handle, uint32_t * src, uint32_t size){
+    if(handle->direction != RINGBUFFER_DIRECTION_TX) return 0;
+    uint32_t available = handle->bufferSize - 1 - DMA_RB_available(handle);
+    if(size > available) size = available;
+    
+    uint32_t currPos = 0;
+    while((handle->lastReadPos != *(handle->channelHandle->DPTR)) && (currPos != size)){
+        handle->data[handle->lastReadPos++] = src[currPos++];
+        if(handle->lastReadPos >= handle->bufferSize) handle->lastReadPos = 0;
+    }
+    
+    return size;
+}
+
 uint32_t DMA_RB_readSB(DMA_RINGBUFFERHANDLE_t * handle, StreamBufferHandle_t buffer, uint32_t size){
+    if(handle->direction != RINGBUFFER_DIRECTION_RX) return 0;
+    
     uint32_t available = DMA_RB_available(handle);
     if(size > available) size = available;
     uint32_t freeSpace = xStreamBufferSpacesAvailable(buffer);
--- include/DMAutils.h
+++ include/DMAutils.h
@@ -5,11 +5,17 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/kmem.h>
+
+#include "FreeRTOS.h"
+#include "stream_buffer.h"
 #include "DMA.h"
+
+#define RINGBUFFER_DIRECTION_RX 0
+#define RINGBUFFER_DIRECTION_TX 1
 
 typedef struct __DMA_RingBuffer_Descriptor__ DMA_RINGBUFFERHANDLE_t;
 
-DMA_RINGBUFFERHANDLE_t * DMA_createRingBuffer(uint32_t bufferSize, uint32_t dataSize, uint32_t * dataSrc, uint32_t dataReadyInt, uint32_t prio);
+DMA_RINGBUFFERHANDLE_t * DMA_createRingBuffer(uint32_t bufferSize, uint32_t dataSize, uint32_t * dataSrc, uint32_t dataReadyInt, uint32_t prio, uint32_t direction);
 void DMA_freeRingBuffer(DMA_RINGBUFFERHANDLE_t * handle);
 
 uint32_t DMA_RB_available(DMA_RINGBUFFERHANDLE_t * handle);
@@ -20,6 +26,7 @@ uint32_t DMA_RB_flush(DMA_RINGBUFFERHANDLE_t * handle);
 struct __DMA_RingBuffer_Descriptor__{
     DMA_HANDLE_t * channelHandle;
     
+    uint32_t direction;
     uint32_t lastReadPos;
     uint32_t bufferSize;
     uint32_t dataSize;
